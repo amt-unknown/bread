34b8076ba545e46308d96ceb401ef99fb1aa5e81
Andrew Thompson <amtunknown@Andrews-MacBook-Pro.local>
2022-03-21 21:04:48 -0700

    sending index and show data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -7,8 +7,12 @@ const breads = express();
 const Bread = require('../models/bread.js');
 
 //INDEX
-breads.get('/:arrayIndex', (req, res) => {
-    res.send(Bread[req.params.arrayIndex]);
+breads.get('/', (req, res) => {
+    res.render('Index',
+    {
+        breads: Bread,
+    })
+    //res.send(Bread)
 });
 
 //EXPORTS
