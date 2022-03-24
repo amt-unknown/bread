1721979f20e7bbc93aba02dd101d9d75a3c37e24
Andrew Thompson <amtunknown@Andrews-MacBook-Pro.local>
2022-03-23 19:52:16 -0700

    sending index and show data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -11,9 +11,22 @@ breads.get('/', (req, res) => {
     res.render('Index',
     {
         breads: Bread,
+        title: 'Index Page'
     })
     //res.send(Bread)
 });
+
+//SHOW
+breads.get('/:arrayIndex', (req, res) => {
+    if (Bread[req.params.arrayIndex]){
+        res.render('Show', {
+            bread: Bread[req.params.arrayIndex],
+        })
+    } else {
+        res.send('404 error: page does not exist')
+    }
+    // res.send(Bread[req.params.arrayIndex])
+})
 
 //EXPORTS
 module.exports = breads;
