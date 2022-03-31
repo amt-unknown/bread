2827cc1dc3253ced3a92847c529480e30f0f2323
Andrew Thompson <amtunknown@Andrews-MacBook-Pro.local>
2022-03-30 20:47:49 -0700

    Mongoosedb connections started

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 //DEPENDENCIES
 const express = require('express');
 const methodOverride = require('method-override')
+const mongoose = require('mongoose')
 
 //CONFIGURATION
 require('dotenv').config();
 const PORT = process.env.PORT;
 const app = express();
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
+    () => {console.log('connected to mongo: ', process.env.MONGO_URI)}
+)
 
 //MIDDLEWARE
 app.use(express.static('public'))
@@ -27,7 +31,7 @@ app.use('/breads', breadsController);
 
 
 
-//Catch all 404 page
+//404 page
 app.get('*', (req,res) => {
     res.status(404).send('404 error: page does not exist');
 });
