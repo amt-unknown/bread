84cc1e8f680b6c2e4fa27a8a5995b72133498d2f
Andrew Thompson <amtunknown@Andrews-MacBook-Pro.local>
2022-04-04 20:27:12 -0700

    updated new and edit to incorporate baker model

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -49,6 +49,7 @@ breads.get('/new', (req, res) => {
 //SHOW
 breads.get('/:id', (req, res) => {
     Bread.findById(req.params.id)
+        .populate('baker')
         .then(foundBread => {
                 const bakedBy = foundBread.getBakedBy()
                 console.log('---------', bakedBy)
@@ -64,11 +65,14 @@ breads.get('/:id', (req, res) => {
 
 //EDIT
 breads.get('/:id/edit', (req,res) => {
-    Bread.findById(req.params.id)
-        .then(foundBread => {
-            // console.log('------ foundBread', foundBread)
-            res.render('edit', {
-                bread: foundBread
+    Baker.find()
+        .then(foundBakers => {
+            Bread.findById(req.params.id)
+                .then(foundBread => {
+                    res.render('edit', {
+                        bread: foundBread,
+                        bakers: foundBakers
+                })
             })
         })
        
--- views/edit.jsx
+++ views/edit.jsx
@@ -1,7 +1,7 @@
 const React = require('react')
 const Default = require('./layouts/Default')
 
-function Edit({bread}){
+function Edit({bread, bakers}){
     return(
         <Default>
             <h2>Edit a bread</h2>
@@ -23,13 +23,12 @@ function Edit({bread}){
                     defaultValue={bread.image}
                 />
                 <label htmlFor="baker">Baker</label>
-                <select name="baker">
-                    <option value="Rachel">Rachel</option>
-                    <option value="Monica">Monica</option>
-                    <option value="Joey">Joey</option>
-                    <option value="Chandler">Chandler</option>
-                    <option value="Ross">Ross</option>
-                    <option value="Phoebe">Phoebe</option>
+                <select name="baker" id="baker" defaultValue={bread.value}>
+                    {bakers.map((baker) => {
+                        return(
+                            <option value={baker.id} key={baker.id}>{baker.name}</option>
+                        )
+                    })}
                 </select>
                 <label htmlFor="hasGluten">Has Gluten?</label>
                 <input 
